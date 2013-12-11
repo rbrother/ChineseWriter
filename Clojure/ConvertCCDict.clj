2267c8d4ed5a837f15257b719734e7a6df88b3bf
Robert Brotherus <robert.brotherus@napa.fi>
2013-12-12 00:35:23 +0200

    combine-duplicates already at ConvertCCDict.clj
    ConvertCCDict.clj to use clojure-clr and Utils

--- Clojure/ConvertCCDict.clj
+++ Clojure/ConvertCCDict.clj
@@ -1,9 +1,11 @@
-; run with java clojure
+; run with clojure-clr. Note that uses my own Utils in clojurecommon.
+; After conversion, consider replacing \" with ' to help pretty-printer later
 
 (ns ConvertCC
   (:require [clojure.string :as str])
   (:use clojure.pprint)
-  (:use clojure.java.io))
+  (:use clojure.set)
+  (:use Utils))
 
 (defn regex-groups [ regex str ]
    (let [ matcher (re-matcher regex str) ]
@@ -35,30 +37,34 @@
          :pinyin-no-spaces-no-tones (remove-tone-numbers pinyin-no-spaces) }
      )))
 
+; cc-dict can contain multiple entries with same hanyu+pinyin but different english,
+; for example 后 Combine those.
+(defn combine-duplicates-inner [values]
+  (assoc (first values) :english (str/join ". " (map :english values))))
+
+(defn combine-duplicates [word-list]
+  (let [ dict (index word-list [ :hanyu :pinyin ]) ]
+    (vals (map-map-values combine-duplicates-inner dict))))
+
 (defn parse-cc-lines [lines]
   (->> lines
     (map #(regex-groups cc-line-regex %))
     (remove nil?)
     (map line-items-to-word)
     (remove is-variant)
-    (map add-word-attributes)
-    (vec)))
+    (map add-word-attributes)))
 
 (defn load-words [ file ]
-  (with-open [rdr (reader file)]
-    (parse-cc-lines (line-seq rdr))))
+  (parse-cc-lines (System.IO.File/ReadAllLines file)))
 
 ; :encoding "UTF-8"
 
 ; We would like to use pprint here, but it turns our to be ~200 times slower thn prn!
 ; So as a compromise use prn line-by-line and generate manually the syntax for the encoding list
 (defn convert-dict [ infile outfile ]
-  (with-open [wri (writer outfile)]
-    (binding [*out* wri]
-      (do
-        (println "[")
-        (dorun (map #(do (pr %) (println ",")) (load-words infile)))
-        (println "]") ))))
+  (let [ converted-words (combine-duplicates (load-words infile))
+         lines (concat ["["] (map pr-str converted-words) ["]"]) ]
+    (System.IO.File/WriteAllLines outfile lines)))
 
 (convert-dict (first *command-line-args*) (nth *command-line-args* 1) )
 
--- Clojure/convert.cmd
+++ Clojure/convert.cmd
@@ -1,1 +1,1 @@
-clojure ConvertCCDict.clj ../cedict_ts_2013-12-06.u8 cedict_ts.clj
+clojure-clr ConvertCCDict.clj ../cedict_ts_2013-12-06.u8 cedict_ts.clj
